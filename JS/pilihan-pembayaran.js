c205fb63e450a21f1c17c7df2487700f0d18e1d8
sadimuslihuddin <74242805+sadimuslihuddin@users.noreply.github.com>
2021-02-14 15:26:09 +0700

    Delete pilihan-pembayaran.js

--- JS/pilihan-pembayaran.js
+++
@@ -1,7 +0,0 @@
-function pilih1() {
-  document.getElementById("pilih").href = "pembayaran-manual.html";
-}
-
-function pilih2() {
-  document.getElementById("pilih").href = "pembayaran-otomatis.html";
-}
